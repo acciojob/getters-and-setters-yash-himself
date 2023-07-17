0bd0de7dad0a721d13e6cb6b42b204d4511b4cda
Yash Gupta <yashgupta27690@gmail.com>
2023-07-17 21:00:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 //complete this code
-class Person {}
+class Person {
+	let name;
+	let age ;
+	constructor(name,age){
+		this.name = name;
+		this.age = age;
+	}
+	get name(){ return this.name}
+	set age(A){this.age = A;}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	constructor(name,age){
+		super(name,age)
+	}
+	study(){
+			console.log(this.name + " is studying");
+		}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	constructor(name,age){
+		super(name,age)
+	}
+	teach(){
+			console.log(this.name + " is teaching");
+		}
+}
 
 // Do not change the code below this line
 window.Person = Person;
